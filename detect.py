913f283fa6ff8da9c47f9f4c36710f94081671a7
RenatoCespedes <71398760+RenatoCespedes@users.noreply.github.com>
2021-08-12 18:57:24 -0500

    as

--- detect.py
+++ detect.py
@@ -160,9 +160,9 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
                 # print("det")
                 # print(len(det))
 
-                # for *xyxy, conf, cls in reversed(det):
-                #     xywh=(xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()
-                #     x_prev=xywh[0]
+                for *xyxy, conf, cls in reversed(det):
+                    xywh=(xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()
+                    x_prev=xywh[0]
                     y_prev=xywh[1]
                     print(x_prev,y_prev)
                 #     vec_y.
